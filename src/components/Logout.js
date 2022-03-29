acfd0dff5f087da8fb8b60f66b3fb33a309d364a
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-03-29 16:11:47 -0400

    completed the logout functionality

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import axiosWithAuth from "../axios";
+
+const logoutURL = "http://localhost:9000/api/logout";
 
 const Logout = () => {
-  return <div>Logout</div>;
+  const { push } = useHistory();
+
+  const logoutHandler = () => {
+    axiosWithAuth()
+      .post(logoutURL)
+      .then((res) => {
+        window.localStorage.removeItem("token");
+        push("/login");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  return (
+    <div className="btn-container">
+      <button onClick={logoutHandler}>Logout</button>
+    </div>
+  );
 };
 
 export default Logout;
